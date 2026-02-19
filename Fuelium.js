0c669bc8b2e960899d9ec796d01209f3742ffae3
hectorxmodex-code <hectorxmodex@gmail.com>
2026-02-19 18:07:05 +0000

    Add files via upload

---
+++ Fuelium.js
@@ -0,0 +1,63 @@
+runAfterLoad(function() {
+
+// =======================
+// GASOLINA
+// =======================
+
+elements.gasolina = {
+    color: "#f5e342",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    state: "liquid",
+    density: 720,
+
+    tempHigh: 110,
+    stateHigh: "gasoline_gas",
+
+    tempLow: 0,
+    stateLow: "gasoline_ice",
+
+    burn: 100,
+    burnTime: 5,
+    burnInto: "fire",
+
+    reactions: {}
+};
+
+// GAS
+elements.gasoline_gas = {
+    color: "#fff3a1",
+    behavior: behaviors.GAS,
+    category: "gases",
+    state: "gas",
+    density: 2,
+
+    tempLow: 109,
+    stateLow: "gasolina",
+
+    burn: 100,
+    burnTime: 5,
+    burnInto: "fire",
+
+    reactions: {}
+};
+
+// HIELO
+elements.gasoline_ice = {
+    color: "#cfefff",
+    behavior: behaviors.WALL,
+    category: "solids",
+    state: "solid",
+    density: 800,
+
+    tempHigh: 1,
+    stateHigh: "gasolina",
+
+    burn: 100,
+    burnTime: 5,
+    burnInto: "fire",
+
+    reactions: {}
+};
+
+});

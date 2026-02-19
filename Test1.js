30f075e487b8d0a27971f367fe280725fec98687
hectorxmodex-code <hectorxmodex@gmail.com>
2026-02-19 18:13:59 +0000

    Create Test1.js

---
+++ Test1.js
@@ -0,0 +1,8 @@
+runAfterLoad(function() {
+    elements.prueba_hector = {
+        color: "#ff0000",
+        behavior: behaviors.WALL,
+        category: "solids",
+        state: "solid"
+    };
+});
